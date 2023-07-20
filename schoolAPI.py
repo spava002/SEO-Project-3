66a87aa7c27681f42c9f11a84912d40c1023f4b8
Sebastian <spava002@fiu.edu>
2023-07-20 14:05:00 -0400

    'Archived' scoolAPI.py as it is no longer needed

--- schoolAPI.py
+++ schoolAPI.py
@@ -1,13 +1,18 @@
-import requests
+'''
+Doesn't serve a purpose anymore, was created simply to demonstrate
+functionality of the API along with how to parse specific data.
+'''
 
-url = 'http://api.data.gov/ed/collegescorecard/v1/schools.json?school.degrees_awarded.predominant=2,3&school.name=Florida%20International%20University'
-API_KEY = 'ZYAlvWEGb09f4OJPE7lH5Z7Y7bQyzw0N72NogF6h'
+# import requests
 
-params = {
-    'api_key': API_KEY
-}
+# url = 'http://api.data.gov/ed/collegescorecard/v1/schools.json?school.degrees_awarded.predominant=2,3&school.name=Florida%20International%20University'
+# API_KEY = 'ZYAlvWEGb09f4OJPE7lH5Z7Y7bQyzw0N72NogF6h'
 
-response = requests.get(url, params=params).json()
+# params = {
+#     'api_key': API_KEY
+# }
+
+# response = requests.get(url, params=params).json()
 
 # # Prints all the general school info
 # print(response["results"][0]["latest"]["school"]["name"])
@@ -19,11 +24,11 @@ response = requests.get(url, params=params).json()
 # print(response["results"][0]["latest"]["aid"])
 
 # Prints all the program titles with their corresponding degree types
-response = response["results"][0]["latest"]["programs"]["cip_4_digit"]
+# response = response["results"][0]["latest"]["programs"]["cip_4_digit"]
 
-i = 0
-while i < len(response):
-    print()
-    print(f"Program: {response[i]['title']}")
-    print(f"Degree: {response[i]['credential']['title']}")
-    i += 1
+# i = 0
+# while i < len(response):
+#     print()
+#     print(f"Program: {response[i]['title']}")
+#     print(f"Degree: {response[i]['credential']['title']}")
+#     i += 1
